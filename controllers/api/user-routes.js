7ec938b4e267c317f813a4ae6c0c61c0170aef96
rongbang <rongbang.ye@gmail.com>
2020-10-25 20:18:57 -0700

    added login and logout page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,25 +41,54 @@ router.post("/", (req, res) => {
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(400).json({ message: "No user with that email address!" });
-        return;
-      }
+  }).then((dbUserData) => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-      const validPassword = dbUserData.checkPassword(req.body.password);
-      if (!validPassword) {
-        res.status(400).json({ message: "Incorrect password!" });
-        return;
-      }
+      res.json(dbUserData);
+    });
+  });
+});
+
+// POST /api/users
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that username!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
       res.json({ user: dbUserData, message: "You are now logged in!" });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
     });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
